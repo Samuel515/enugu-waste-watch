767b004d2cb3b6408b2b29327930ebaf061eb0e7
Samuel Ogbodo <samuelogbodo515@gmail.com>
2025-05-21 13:07:07 +0100

    more fix

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -304,7 +304,7 @@ const Notifications = () => {
                 {/* Display upcoming collections at the top */}
                 {collectionNotifications.length > 0 && (
                   <div className="mb-4">
-                    <h3 className="text-lg font-medium mb-2">Upcoming Collections (Next 24 Hours)</h3>
+                    <h3 className="text-lg font-medium mb-2">Upcoming Collections</h3>
                     <NotificationList 
                       notifications={collectionNotifications}
                       onMarkAsRead={async () => {}}
